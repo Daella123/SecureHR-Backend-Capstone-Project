1d9c8f43a9f9a248c1b0729c34dcbdc6ba52c073
Daella123 <daellsine@gmail.com>
2025-12-01 11:22:15 +0200

    added server

--- src/main/java/com/daella/employeemanagement/config/OpenApiConfig.java
+++ src/main/java/com/daella/employeemanagement/config/OpenApiConfig.java
@@ -5,13 +5,24 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(title = "Employee Management API", version = "1.0", description = "RESTful API for managing employees with JWT authentication"),
-        security = @SecurityRequirement(name = "bearerAuth")
+        security = @SecurityRequirement(name = "bearerAuth"),
+        servers = {
+                @Server(
+                        url = "http://localhost:8080",
+                        description = "Local Server"
+                ),
+                @Server(
+                        url = "securehr-backend-capstone-project-production.up.railway.app",
+                        description = "Production Server"
+                )
+        }
 )
 @SecurityScheme(
         name = "bearerAuth",
